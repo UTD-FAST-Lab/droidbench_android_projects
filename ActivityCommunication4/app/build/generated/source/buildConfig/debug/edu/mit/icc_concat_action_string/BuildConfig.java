96461dbe02bb5004c6e04510b8d42146f27dfa48
pancax <dakotasoles@gmail.com>
2021-08-05 03:41:34 -0500

    init commit

---
+++ ActivityCommunication4/app/build/generated/source/buildConfig/debug/edu/mit/icc_concat_action_string/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.mit.icc_concat_action_string;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.mit.icc_concat_action_string";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
